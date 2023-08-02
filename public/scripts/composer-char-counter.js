2ed4d73055d6ec438a62ac2cacb75d0dc31202b2
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-08-02 23:38:04 +0000

    update relative url and also stored max tweets using MAX_TWEET_LENGTH

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,4 +1,6 @@
-    $(document).ready(function() {
+const MAX_TWEET_LENGTH = 140;
+   
+   $(document).ready(function() {
         // Get the textarea DOM node using the same selector
         const textarea = document.querySelector('.text');
   
@@ -10,15 +12,13 @@
   
         // Registering an event handler for the textarea element
         $textarea.on("input", function() {
-          const charLimit = 140; // Set the character limit here
           const inputValue = $textarea.val(); // Get the value of the textarea using jQuery
           const currentLength = inputValue.length;
-          const remainingChars = charLimit - currentLength;
+          const remainingChars = MAX_TWEET_LENGTH - currentLength;
   
           // Update the content of the output element with the remaining characters
           $counter.text(remainingChars);
   
-          // Add the "invalid" class to the counter if remainingChars is negative
           if (remainingChars < 0) {
             $counter.addClass("invalid");
           } else {
