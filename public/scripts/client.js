c81e97c7652bf0dadd1d0765f98d186ea1387559
Teniola Ogunlaja <isaacogunlaja9@gmail.com>
2023-08-07 19:58:19 +0000

    reset the couter upon each tweets

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -30,86 +30,92 @@ const data = [
     }
 ]
 
-const loadTweets = function () {
-    $.ajax({
-        url: 'http://localhost:8080/tweets',
-        method: 'GET',
-        dataType: 'json',
-        success: function (tweets) {
-            // Handle the received array of tweets here
-            console.log('Received tweets:', tweets);
 
-            // Call the function to render the tweets
-            renderTweets(tweets);
-        },
-        error: function (error) {
-            // Handle any errors that occur during the request
-            console.error('Error loading tweets:', error);
-        }
-    });
-};
-
-// XSS protection using escape function
-const escape = function (str) {
-    const div = document.createElement('div');
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML;
-};
-
-
-
-// Define the createTweetElement function
-function createTweetElement(tweetData) {
-    // Extract tweet data from the tweet object
-    const { user, content, created_at } = tweetData;
-
-    // Create HTML elements for the tweet structure
-    const $article = $('<article>').addClass('tweet');
-    const $header = $('<header>').addClass('posted-tweet');
-    const $div = $('<div>').attr('id', 'rhonda');
-    const $img = $('<img>').attr('src', escape(user.avatars));
-    const $name = $('<p>').text(escape(user.name));
-    const $handle = $('<p>').text(escape(user.handle));
-    const $content = $('<p>').addClass('hello').html(escape(content.text)); // Use html() to display escaped HTML
-    const $footer = $('<footer>').addClass('tweets-foot');
-    const $timeAgo = $('<span>').addClass('time-ago').text(timeago.format(new Date(created_at)));
-    const $iconsDiv = $('<div>').addClass('gif');
-    const $flagIcon = $('<i>').addClass('fa-solid fa-flag');
-    const $retweetIcon = $('<i>').addClass('fa-solid fa-retweet');
-    const $heartIcon = $('<i>').addClass('fa-solid fa-heart');
-
-    // Append the HTML elements to build the tweet structure
-    $div.append($img, $name);
-    $header.append($div, $handle);
-    $footer.append($timeAgo, $iconsDiv);
-    $iconsDiv.append($flagIcon, $retweetIcon, $heartIcon);
-    $article.append($header, $content, $footer);
-
-    // Return the complete tweet article element
-    return $article;
-}
-
-
-const renderTweets = function (tweets) {
-    // Loop through the tweets array
-    tweets.forEach(function (tweet) {
-        // Call the createTweetElement function for each tweet
-        const $tweetElement = createTweetElement(tweet);
-        // Append the returned jQuery object to the #tweets-container section
-        $('#tweets-container').prepend($tweetElement);
-    });
-};
 
 $(document).ready(function () {
-    // Call loadTweets when the document is ready to fetch and render tweets
-    loadTweets();
 
-    // event listener for submit button
-    $('.tweet-button').click(function (event) {
+    const loadTweets = function () {
+        $.ajax({
+            url: 'http://localhost:8080/tweets',
+            method: 'GET',
+            dataType: 'json',
+            success: function (tweets) {
+                // Handle the received array of tweets here
+                console.log('Received tweets:', tweets);
+    
+                // Call the function to render the tweets
+                renderTweets(tweets);
+            },
+            error: function (error) {
+                // Handle any errors that occur during the request
+                console.error('Error loading tweets:', error);
+            }
+        });
+    };
+    
+    // XSS protection using escape function
+    const escape = function (str) {
+        const div = document.createElement('div');
+        div.appendChild(document.createTextNode(str));
+        return div.innerHTML;
+    };
+    
+    
+    
+    // Define the createTweetElement function
+    function createTweetElement(tweetData) {
+        // Extract tweet data from the tweet object
+        const { user, content, created_at } = tweetData;
+        const $tweetContainer = $('<div>').addClass('tweet-container');
+    
+        // Create HTML elements for the tweet structure
+        const $article = $('<article>').addClass('tweet');
+        const $header = $('<header>').addClass('posted-tweet');
+        const $div = $('<div>').attr('id', 'rhonda');
+        const $img = $('<img>').attr('src', escape(user.avatars));
+        const $name = $('<p>').text(escape(user.name));
+        const $handle = $('<p>').text(escape(user.handle));
+        const $content = $('<p>').addClass('hello').html(escape(content.text)); // Use html() to display escaped HTML
+        const $footer = $('<footer>').addClass('tweets-foot');
+        const $timeAgo = $('<span>').addClass('time-ago').text(timeago.format(new Date(created_at)));
+        const $iconsDiv = $('<div>').addClass('gif');
+        const $flagIcon = $('<i>').addClass('fa-solid fa-flag');
+        const $retweetIcon = $('<i>').addClass('fa-solid fa-retweet');
+        const $heartIcon = $('<i>').addClass('fa-solid fa-heart');
+    
+        // Append the HTML elements to build the tweet structure
+        $div.append($img, $name);
+        $header.append($div, $handle);
+        $footer.append($timeAgo, $iconsDiv);
+        $iconsDiv.append($flagIcon, $retweetIcon, $heartIcon);
+        $article.append($header, $content, $footer);
+        $tweetContainer.append($article);
+    
+        // Return the complete tweet article element
+        return $article;
+    }
+    
+    
+    const renderTweets = function (tweets) {
+        // Loop through the tweets arrayconst 
+        $tweetsContainer = $('#tweets-container');
+    
+        // Clear existing tweets before adding new ones
+        $tweetsContainer.empty();
+    
+        tweets.forEach(function (tweet) {
+            // Call the createTweetElement function for each tweet
+            const $tweetElement = createTweetElement(tweet);
+            // Append the returned jQuery object to the #tweets-container section
+            $('#tweets-container').prepend($tweetElement);
+        });
+    };
+
+    const handleTweetSubmit = function (event) {
         // Prevent default form submission behavior
         event.preventDefault();
         console.log("Form submitted!");
-        
+
         // Ajax submit event    
         const tweetContent = $('#tweet-text').val();
 
@@ -126,6 +132,7 @@ $(document).ready(function () {
             // If content is valid, clear the error message and submit the form
             clearError();
 
+
             // Serialize the form data
             const formData = $("form").serialize();
 
@@ -134,71 +141,34 @@ $(document).ready(function () {
                 type: 'POST',
                 url: '/tweets',
                 data: formData,
-                success: function (response) {
-                    // Handle the response from the server (if needed)
-                    console.log('Form submitted successfully:', response);
+                
+            })
+            .then(function (response) {
+                // Handle the response from the server (if needed)
+                console.log('Form submitted successfully:', response);
 
-                    // Clear the textarea after successful submission
-                    $('#tweet-text').val('');
+                // Clear the textarea after successful submission
+                $('#tweet-text').val('');
+                
 
-                    // Optionally, reload the tweets to display the new tweet
-                    loadTweets();
-                },
-                error: function (error) {
-                    // Handle any errors that occur during the POST request (if needed)
-                    console.error('Error submitting form:', error);
-                }
-            });
+                // Optionally, reload the tweets to display the new tweet
+                loadTweets();
+                const $counter  = $('.counter');
+                //const MAX_TWEET_LENGTH = 140;
+                $counter.text(MAX_TWEET_LENGTH);
+               
+            })
+
         }
-    });
+    }
+    // Call loadTweets when the document is ready to fetch and render tweets
+    loadTweets();
+
+    // event listener for submit button
+    $('.tweet-button').click(handleTweetSubmit);
 
     // event listener for form submission
-    $('.form').submit(function (event) {
-        // Prevent default form submission behavior
-        event.preventDefault();
-
-        // Get the tweet content from the textarea
-        const tweetContent = $('#tweet-text').val();
-
-        // Hide error message when form submission
-        hideError();
-
-        // Check if the tweet content is empty or too long
-        if (!tweetContent) {
-            // Show error message for empty content
-            showError("Tweet content cannot be empty.");
-        } else if (tweetContent.length > 140) {
-            // Show error message for content that is too long
-            showError("Tweet content exceeds the maximum length of 140 characters.");
-        } else {
-            // If content is valid, clear the error message and submit the form
-            clearError();
-
-            // Serialize the form data
-            const formData = $(this).serialize();
-
-            // Send a POST request with the serialized data to the server
-            $.ajax({
-                type: 'POST',
-                url: '/tweets',
-                data: formData,
-                success: function (response) {
-                    // Handle the response from the server (if needed)
-                    console.log('Form submitted successfully:', response);
-
-                    // Clear the textarea after successful submission
-                    $('#tweet-text').val('');
-
-                    // Optionally, reload the tweets to display the new tweet
-                    loadTweets();
-                },
-                error: function (error) {
-                    // Handle any errors that occur during the POST request (if needed)
-                    console.error('Error submitting form:', error);
-                }
-            });
-        }
-    });
+    $('.form').submit(handleTweetSubmit);
 
     // Helper function to show the error message
     function showError(message) {
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,28 +1,46 @@
 const MAX_TWEET_LENGTH = 140;
-   
-   $(document).ready(function() {
-        // Get the textarea DOM node using the same selector
-        const textarea = document.querySelector('.text');
-  
-        // Convert the textarea DOM node to a jQuery object
-        const $textarea = $(textarea);
-  
-        // Get the output element using the same selector
-        const $counter = $('.counter');
-  
-        // Registering an event handler for the textarea element
-        $textarea.on("input", function() {
-          const inputValue = $textarea.val(); // Get the value of the textarea using jQuery
-          const currentLength = inputValue.length;
-          const remainingChars = MAX_TWEET_LENGTH - currentLength;
-  
-          // Update the content of the output element with the remaining characters
-          $counter.text(remainingChars);
-  
-          if (remainingChars < 0) {
-            $counter.addClass("invalid");
-          } else {
-            $counter.removeClass("invalid");
-          }
-        });
-      });
+
+$(document).ready(function () {
+  // Get the textarea DOM node using the same selector
+  //const textarea = document.querySelector('.text');
+
+  // Convert the textarea DOM node to a jQuery object
+  const $textarea = $('.text');
+
+  // Get the output element using the same selector
+  const $counter = $('.counter');
+
+  // Get the max tweet length from the data attribute
+  //const MAX_TWEET_LENGTH = parseInt($textarea.attr('data-max-length'), 10);
+
+  // Set the max tweet length in the output element
+  $counter.text(MAX_TWEET_LENGTH);
+
+  // Set the max tweet length in the error message
+  $('#max-tweet-length').text(MAX_TWEET_LENGTH);
+  const updateCounter = function () {
+    const inputValue = $('.text').val(); // Get the value of the textarea using jQuery
+    const currentLength = inputValue.length;
+    const remainingChars = MAX_TWEET_LENGTH - currentLength;
+
+    // Update the content of the output element with the remaining characters
+    $('.counter').text(remainingChars);
+
+    if (remainingChars < 0) {
+      $('.counter').addClass("invalid");
+    } else {
+      $('.counter').removeClass("invalid");
+    }
+  }
+  // Registering an event handler for the textarea element
+  $textarea.on("input", updateCounter);
+
+   // Hover effect for the tweet container
+   $('.tweet-container').hover(function () {``
+    $(this).addClass('tweet-container-shadow');
+  }, function () {
+    $(this).removeClass('tweet-container-shadow');
+  });
+});
+
+
